91a545c567456c082df8edf3431b1c6e380aa27f
laure-seng <laure-seng@oclock.school>
2024-01-04 21:05:23 +0100

    corecontroller avec namespaces et composer.json inclue autoload psr4

---
+++ app/Controllers/CoreController.php
@@ -0,0 +1,7 @@
+<?php
+// //Exemples de nameSpaces
+// use  Oshop\Controllers\CoreController ;
+// use  Oshop\Models\Category ; 
+// use  Oshop\Models\Type ; 
+// use  Oshop\Models\Brand ; 
+// use  Oshop\Models\Product ;  
